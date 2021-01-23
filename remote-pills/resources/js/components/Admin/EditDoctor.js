012ff6368bad81012c8dd7b13b536404239e23b6
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-23 17:16:33 +0200

    Edit Doctor

--- remote-pills/resources/js/components/Admin/EditDoctor.js
+++ remote-pills/resources/js/components/Admin/EditDoctor.js
@@ -7,11 +7,10 @@ import Back from '../Back';
 
 export  default function EditDoctor(props){
 
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [email, setEmail] = useState('');
-    const [errors, setErrors] = useState([]);
-
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [email, setEmail] = useState('');
+  const [errors, setErrors] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
@@ -20,153 +19,122 @@ export  default function EditDoctor(props){
         setName(response.data.name);
         setEmail(response.data.email);
 
-      })
-
-
+    })
   },[]);
 
 
   function hasErrorFor (field) {
     return !!errors[field]
-}
+  }
 
-function renderErrorFor (field) {
+  function renderErrorFor (field) {
     if (hasErrorFor(field)) {
-        return (
-            <span style={{ color: '#D7425C' }}>
-                <strong>{errors[field][0]}</strong>
-            </span>
-        )
+      return (
+          <span style={{ color: '#D7425C' }}>
+              <strong>{errors[field][0]}</strong>
+          </span>
+      )
     }
-}
+  }
 
 
 
-function handleNameChange(event){
-  setName(event.target.value);
-}
+  function handleNameChange(event){
+    setName(event.target.value);
+  }
 
 
 
-function handleFormatChange(event){
-  setFormat(event.target.value);
-}
+  function handleFormatChange(event){
+    setFormat(event.target.value);
+  }
 
-function handleDescriptionChange(event){
-  setDescription(event.target.value);
-}
-
-
-function handleIngredientChange(event){
-  setIngredient(event.target.value);
-}
+  function handleDescriptionChange(event){
+    setDescription(event.target.value);
+  }
 
 
-function handlePrescriptionChange(event){
-  if(event.target.checked){
-    setPrescription(1)
-}else{
-    setPrescription(0)
-}
-}
+  function handleIngredientChange(event){
+    setIngredient(event.target.value);
+  }
 
 
-function handleTabletChange(event){
-  setTablet(event.target.value);
-}
-
-
-function handleDosageChange(event){
-  setDosage(event.target.value);
-}
-
-function handleUnitChange(event){
-  setUnit(event.target.value);
-}
-
-
-
+  function handlePrescriptionChange(event){
+    if(event.target.checked){
+      setPrescription(1)
+    }else{
+        setPrescription(0)
+    }
+  }
 
    
-function handleUpdateMedicine(event) {
-  event.preventDefault();
+  function handleUpdateMedicine(event) {
+    event.preventDefault();
 
 
-  const fd = new FormData();
-        fd.append('name', name);
+    const fd = new FormData();
+          fd.append('name', name);
 
-        const medicine = {
-          name: name
-      }
-    
-
-  api.updatePharmacy(medicine, props.match.params.id  ) //, {headers:{'Accept': "application/x-www-form-urlencoded"}}
-      .then(response => {
-        alert("update success");
-
-        history.push('/managePharmacy')
-        window.location.reload();
-      })
-      .catch(error => {
-        console.log(error)
-        //  setErrors(error.response.data.errors)
-      })
-}
-
-    return(
-      <div className="templatemo-flex-row">
-	  
-      <div className="templatemo-content col-1 light-gray-bg">
+          const medicine = {
+            name: name
+    }
       
-       <Header />
-        <div className="templatemo-flex-row flex-content-row " style={{ marginTop:'100px' }}>
-            <div className="col-1">	
-            <Back  />
+    api.updatePharmacy(medicine, props.match.params.id)
+    .then(response => {
+      alert("update success");
 
-        
-    <div className="templatemo-content-widget templatemo-login-widget  white-bg">
+      history.push('/managePharmacy')
+      window.location.reload();
+    })
+    .catch(error => {
+      console.log(error)
+      //  setErrors(error.response.data.errors)
+    })
+  }
 
-    <img src={image} alt="Image"  style={{ width:'50px' , height:'50px'}}/><br/><br/>
-          <div className={`form-group ${hasErrorFor('name') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">name</div>	        		
-            <input type="text" className="form-control"
-            value={name} onChange={handleNameChange}
-            />  
-		          	</div>
-                {renderErrorFor('name')} 
-                </div>
-
-                <div className="form-group" >
-          <div className="input-group" >
-          <i className="fa fa-envelope-o"></i> &nbsp;
-            {email} 
-		          	</div>
-                </div>
-            
-        
-            <div className="form-group">
-					<button type="submit" className="templatemo-blue-button width-100"
-          onClick={(event) => handleUpdateMedicine(event)}
-					 >
-					Update </button>
-				</div>
-
-
-
+  return(
+    <div className="templatemo-flex-row">
+  
+      <div className="templatemo-content col-1 light-gray-bg">
     
-          </div>
-        </div>   
-        </div> 
+        <Header />
+        <div className="templatemo-flex-row flex-content-row " style={{ marginTop:'100px' }}>
+          <div className="col-1">	
+            <Back  />
+            <div className="templatemo-content-widget templatemo-login-widget  white-bg">
 
-            
-                     
-<Footer />
-</div>
+              <img src={image} alt="Image"  style={{ width:'50px' , height:'50px'}}/>
 
+              <br/><br/>
+              <div className={`form-group ${hasErrorFor('name') ? 'has-error' : ''}`} >
+                <div className="input-group" >
+                    <div className="input-group-addon">name</div>	        		
+                      <input type="text" className="form-control"
+                      value={name} onChange={handleNameChange}/>  
+                </div>
+                {renderErrorFor('name')} 
+              </div>
 
-</div> 
+              <div className="form-group" >
+                <div className="input-group" >
+                  <i className="fa fa-envelope-o"></i> &nbsp;
+                  {email} 
+                </div>
+              </div>
+              
+          
+              <div className="form-group">
+                <button type="submit" className="templatemo-blue-button width-100"
+                onClick={(event) => handleUpdateMedicine(event)}>
+                Update </button>
+              </div>
+            </div>
+          </div>   
+        </div>                    
+        <Footer />
+      </div>
+    </div> 
 
-    )
+  )
 
 }
