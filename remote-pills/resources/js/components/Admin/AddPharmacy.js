5ad910e5906eaa0c6b14a661050179a87a933c10
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-23 14:43:23 +0200

    Add Pharmacy

--- remote-pills/resources/js/components/Admin/AddPharmacy.js
+++ remote-pills/resources/js/components/Admin/AddPharmacy.js
@@ -20,13 +20,11 @@ export  default function AddPharmacy(props){
 
   const history = useHistory();
 
-
-
   function hasErrorFor (field) {
     return !!errors[field]
-}
+  }
 
-function renderErrorFor (field) {
+  function renderErrorFor (field) {
     if (hasErrorFor(field)) {
         return (
             <span style={{ color: '#D7425C' }}>
@@ -34,211 +32,175 @@ function renderErrorFor (field) {
             </span>
         )
     }
-}
+  }
 
 
 
 
-function handleNameChange(event){
-  setName(event.target.value);
-}
+  function handleNameChange(event){
+    setName(event.target.value);
+  }
 
 
-function handleImageChange(event){
-  setImage(event.target.files[0]); 
-}
+  function handleImageChange(event){
+    setImage(event.target.files[0]); 
+  }
 
 
-function handleFormatChange(event){
-  setFormat(event.target.value);
-}
+  function handleFormatChange(event){
+    setFormat(event.target.value);
+  }
 
-function handleDescriptionChange(event){
-  setDescription(event.target.value);
-}
-
-
-function handleIngredientChange(event){
-  setIngredient(event.target.value);
-}
+  function handleDescriptionChange(event){
+    setDescription(event.target.value);
+  }
 
 
-function handlePrescriptionChange(event){
-  if(event.target.checked){
-    setPrescription(1)
-}else{
-    setPrescription(0)
-}
-}
+  function handleIngredientChange(event){
+    setIngredient(event.target.value);
+  }
 
 
-function handleTabletChange(event){
-  setTablet(event.target.value);
-}
+  function handlePrescriptionChange(event){
+    if(event.target.checked){
+      setPrescription(1)
+    }else{
+        setPrescription(0)
+    }
+  }
 
 
-function handleDosageChange(event){
-  setDosage(event.target.value);
-}
-
-function handleUnitChange(event){
-  setUnit(event.target.value);
-}
+  function handleTabletChange(event){
+    setTablet(event.target.value);
+  }
 
 
+  function handleDosageChange(event){
+    setDosage(event.target.value);
+  }
 
+  function handleUnitChange(event){
+    setUnit(event.target.value);
+  }
 
    
-function handleAddMedicine(event) {
-  event.preventDefault();
+  function handleAddMedicine(event) {
+    event.preventDefault();
+    const fd = new FormData();
+    fd.append('image', image);
+    fd.append('name', name);
+    fd.append('format', format);
+    fd.append('description', description);
+    fd.append('ingredient', ingredient);
+    fd.append('prescription', prescription);
+    fd.append('tablet', tablet);
+    fd.append('dosage', dosage);
+    fd.append('dosage_unit', unit);
+    api.addMedicineInfo(fd , {headers:{'Accept': "application/json",  'Content-Type': "multipart/form-data"}})
+    .then(response => {
+      alert("add success");
 
+      history.push('/manageMedicine')
+      window.location.reload();
+    })
+    .catch(error => {
+      console.log(error)
+      setErrors(error.response.data.errors)
+    })
+  }
 
-  const fd = new FormData();
-        fd.append('image', image);
-        fd.append('name', name);
-        fd.append('format', format);
-        fd.append('description', description);
-        fd.append('ingredient', ingredient);
-        fd.append('prescription', prescription);
-        fd.append('tablet', tablet);
-        fd.append('dosage', dosage);
-        fd.append('dosage_unit', unit);
-    
+  return(
+    <div className="col-1">	
+      <div style={{ marginTop:'10px' }} ></div>
 
-  api.addMedicineInfo(fd , {headers:{'Accept': "application/json",  'Content-Type': "multipart/form-data"}})
-      .then(response => {
-        alert("add success");
-        // setName('');
-        // setImage('');
-        // setFormat('Tablet');
-        // setDescription('');
-        // setIngredient('');
-        // setPrescription(0);
-        // setTablet('');
-        // setDosage('');
-        // setUnit('');
-
-        history.push('/manageMedicine')
-        window.location.reload();
-      })
-      .catch(error => {
-        console.log(error)
-        setErrors(error.response.data.errors)
-      })
-}
-
-    return(
-              <div className="col-1">	
-
-    <div style={{ marginTop:'10px' }} >
-        </div>
-
-    <div className="templatemo-content-widget templatemo-login-widget  white-bg">
-    <div className="scrollform">
-    <div className="form-group">
-          <div className="input-group" >
-            <div className="input-group-addon">image</div>	        		
-            <input type="file" className="form-control"
-              onChange={handleImageChange}
-               />   
-		          	</div>
-                </div>
+      <div className="templatemo-content-widget templatemo-login-widget  white-bg">
+        <div className="scrollform">
+          <div className="form-group">
+              <div className="input-group" >
+                <div className="input-group-addon">image</div>	        		
+                <input type="file" className="form-control"
+                  onChange={handleImageChange}/>   
+              </div>
+          </div>
 
           <div className={`form-group ${hasErrorFor('name') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">name</div>	        		
-            <input type="text" className="form-control"
-            value={name} onChange={handleNameChange}
-            />  
-		          	</div>
-                {renderErrorFor('name')} 
-                </div>
-
-
-                <div className={`form-group ${hasErrorFor('email') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">format</div>	 
-            <select className="form-control"value={format} onChange={handleFormatChange} >
-              <option value="Tablet">Tablet</option>
-              <option value="Liquid">Liquid</option>
-              <option value="Cream">Cream</option>
-            </select>
-		          	</div>
-                {renderErrorFor('email')} 
-                </div>
-
-
-                
-
-                <div className={`form-group ${hasErrorFor('password') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">description</div>	        		
-            <textarea type="text" className="form-control"
-             value={description} onChange={handleDescriptionChange}
-
-               />   
-		          	</div>
-                {renderErrorFor('password')} 
-                </div>
-
-                <div className={`form-group ${hasErrorFor('ingredient') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">ingredient</div>	        		
-            <textarea type="text" className="form-control"
-             value={ingredient} onChange={handleIngredientChange}
-               />   
-		          	</div>
-                {renderErrorFor('ingredient')} 
-                </div>
-
-                <div className={`form-group ${hasErrorFor('tablet') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">tablet</div>	        		
-            <input type="number" className="form-control"
-             value={tablet} onChange={handleTabletChange}
-               />   
-		          	</div>
-                {renderErrorFor('tablet')} 
-                </div>
-
-                <div className={`form-group ${hasErrorFor('dosage') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">dosage</div>	        		
-            <input type="number" className="form-control"
-             value={dosage} onChange={handleDosageChange}
-               />   
-		          	</div>
-                {renderErrorFor('dosage')} 
-                </div>
-
-                <div className={`form-group ${hasErrorFor('dosage_unit') ? 'has-error' : ''}`} >
-          <div className="input-group" >
-            <div className="input-group-addon">dosage unit</div>	        		
-            <input type="text" className="form-control"
-               value={unit} onChange={handleUnitChange}
-               />   
-		          	</div>
-                {renderErrorFor('dosage_unit')} 
-                </div>
-
-        
-
-
+            <div className="input-group" >
+              <div className="input-group-addon">name</div>	        		
+              <input type="text" className="form-control"
+              value={name} onChange={handleNameChange}/>  
             </div>
-        
-            <div className="form-group">
-					<button type="submit" className="templatemo-blue-button width-100"
-          onClick={(event) => handleAddMedicine(event)}
-					 >
-					Add </button>
-				</div>
-
-
-
-    
+            {renderErrorFor('name')} 
           </div>
-        </div>    
 
-    )
 
+          <div className={`form-group ${hasErrorFor('email') ? 'has-error' : ''}`} >
+            <div className="input-group" >
+              <div className="input-group-addon">format</div>	 
+                <select className="form-control"value={format} onChange={handleFormatChange} >
+                  <option value="Tablet">Tablet</option>
+                  <option value="Liquid">Liquid</option>
+                  <option value="Cream">Cream</option>
+                </select>
+              </div>
+              {renderErrorFor('email')} 
+          </div>
+
+
+                    
+
+          <div className={`form-group ${hasErrorFor('password') ? 'has-error' : ''}`} >
+            <div className="input-group" >
+              <div className="input-group-addon">description</div>	        		
+              <textarea type="text" className="form-control"
+                value={description} onChange={handleDescriptionChange}/>   
+            </div>
+            {renderErrorFor('password')} 
+          </div>
+
+          <div className={`form-group ${hasErrorFor('ingredient') ? 'has-error' : ''}`} >
+            <div className="input-group" >
+              <div className="input-group-addon">ingredient</div>	        		
+              <textarea type="text" className="form-control"
+              value={ingredient} onChange={handleIngredientChange}/>   
+            </div>
+            {renderErrorFor('ingredient')} 
+          </div>
+
+          <div className={`form-group ${hasErrorFor('tablet') ? 'has-error' : ''}`} >
+            <div className="input-group" >
+              <div className="input-group-addon">tablet</div>	        		
+                <input type="number" className="form-control"
+                value={tablet} onChange={handleTabletChange}/>   
+            </div>
+              {renderErrorFor('tablet')} 
+          </div>
+
+          <div className={`form-group ${hasErrorFor('dosage') ? 'has-error' : ''}`} >
+            <div className="input-group" >
+              <div className="input-group-addon">dosage</div>	        		
+              <input type="number" className="form-control"
+                value={dosage} onChange={handleDosageChange} />   
+            </div>
+            {renderErrorFor('dosage')} 
+          </div>
+
+          <div className={`form-group ${hasErrorFor('dosage_unit') ? 'has-error' : ''}`} >
+            <div className="input-group" >
+              <div className="input-group-addon">dosage unit</div>	        		
+              <input type="text" className="form-control"
+              value={unit} onChange={handleUnitChange} />   
+            </div>
+            {renderErrorFor('dosage_unit')} 
+          </div>
+        </div>
+          
+        <div className="form-group">
+          <button type="submit" className="templatemo-blue-button width-100"
+          onClick={(event) => handleAddMedicine(event)}>
+          Add 
+          </button>
+        </div>
+      </div>
+    </div>    
+  )
 }
