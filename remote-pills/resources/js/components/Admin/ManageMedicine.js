49a9d10215be3369f1d3dd3867c746ac817f44e1
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-24 19:13:59 +0200

    Manage Medicine

--- remote-pills/resources/js/components/Admin/ManageMedicine.js
+++ remote-pills/resources/js/components/Admin/ManageMedicine.js
@@ -10,10 +10,6 @@ import AddMedicine from './AddMedicine';
 export  default function ManageMedicine(){
   const [id, setId] = useState('');
   const [medicine, setMedicine] = useState([]);
-
-  
-
-
   const[pages, setPages] = useState([]);
 
   useEffect(() => {
@@ -24,35 +20,35 @@ export  default function ManageMedicine(){
 
       setPages(response.data);
     })
- },[]);
+  },[]);
 
- function filterFunction(event){
+  function filterFunction(event){
     var search = event.target.value;
     // Declare variables
-var input, filter, table, tr, th, td, i ;
-input = document.getElementById("myInput");
-filter = search.toUpperCase();
-table = document.getElementById("myTable");
-tr = table.getElementsByTagName("tr"),
-th = table.getElementsByTagName("th");
+    var input, filter, table, tr, th, td, i ;
+    input = document.getElementById("myInput");
+    filter = search.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr"),
+    th = table.getElementsByTagName("th");
 
-// Loop through all table rows, and hide those who don't match the        search query
-for (i = 1; i < tr.length; i++) {
-            tr[i].style.display = "none";
-            for(var j=0; j<th.length; j++){
+    // Loop through all table rows, and hide those who don't match the        search query
+    for (i = 1; i < tr.length; i++) {
+      tr[i].style.display = "none";
+      for(var j=0; j<th.length; j++){
         td = tr[i].getElementsByTagName("td")[j];      
         if (td) {
           var txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-                break;
-            }
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+            break;
+          }
         }
-        }
+      }
     }
-   }
+  }
 
-   function sortTable(n) {
+  function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("myTable");
     switching = true;
@@ -110,13 +106,13 @@ for (i = 1; i < tr.length; i++) {
    function addMedicine(){
     if (document.getElementById("addmedicine").style.display =="block"){
       document.getElementById("addmedicine").style.display="none";} 
-      else{
-      document.getElementById("addmedicine").style.display = "block";
-      }
-   }
+    else{
+    document.getElementById("addmedicine").style.display = "block";
+    }
+  }
 
 
-   function handleDeleteMedicine(medicine_id){ //I added event here and in line 89
+  function handleDeleteMedicine(medicine_id){ //I added event here and in line 89
     event.preventDefault();
     var confirm_delete = confirm('Are you sure you want to Delete Medicine?');
     if (confirm_delete == true) {
@@ -124,7 +120,7 @@ for (i = 1; i < tr.length; i++) {
             window.location.reload();
         })
     }
-}
+  }
 
   function nextPage(url){
     api.getAllMedicine(url).then(response => {
@@ -136,169 +132,154 @@ for (i = 1; i < tr.length; i++) {
 
    function renderMedicine(){
     return medicine.map(medicine => {
-        return(
-          <tr key={medicine.id}>
-                    <td>{medicine.id}</td>
-                    <td>
-                    
-                    <img src={medicine.image} 
-                    style={{objectFit: 'contain' }} width="100px" height="100px"/></td>
-                    <td>{medicine.name}</td>
-                    <td>
-                      <div style={{ overflowY:'auto', width:'200px' , height: '100px', whiteSpace: 'pre-line' }}>
-                      {medicine.description}
-                      </div>
-                    </td>
-                    <td>
-                    <div style={{ overflowY:'auto', width:'200px', height: '100px', whiteSpace: 'pre-line' }}>
-
-                    {medicine.ingredient}
-                    </div>
-                    
-                    </td>
-                    <td>{medicine.format}</td>
-                    <td>{medicine.prescription == 1 ? 'yes' : 'no'}</td>
-                    <td>{medicine.tablet}</td>
-                    <td>{medicine.dosage} {medicine.dosage_unit}</td>
-                    <td style={{ textAlign:'center'}}>
-                    <div className="btn-group " role="group">
-                    
-                               
-                               <a href={`/manageMedicine/show/${medicine.id}`}
-                               className="btn btn-info"
-                                   title="Show">
-                                   <i className="fa fa-info fa-fw"></i>
-                                </a>
-                                <a href={`/manageMedicine/edit/${medicine.id}`}
-                                className="btn btn-primary"
-                                   title="Edit">
-                                   <i className="fa fa-edit fa-fw"></i>
-                                </a>
-                            
-                                <a  onClick= {() => handleDeleteMedicine(medicine.id)}
-                                 className="btn btn-default"
-                                   title="Delete">
-                                   <i className="fa fa-trash fa-fw"></i>
-                                </a>
-                            </div>
-                            </td>
-                  </tr>
-          )
-        })
-    }
-  
- 
-
-
-
-
-
-
-
-
-  
-    return(
-        <div className="templatemo-flex-row">
-	  
-        <div className="templatemo-content col-1 light-gray-bg">
-        
-         <Header />
-          <div className="templatemo-flex-row flex-content-row " style={{ marginTop:'100px' }}>
-              <div className="col-1">	
-           
-              <div className="templatemo-content-widget no-padding">
-            <div className="panel panel-default table-responsive">
-            <table id="myTable" className="table table-striped table-bordered templatemo-user-table"
-             cellSpacing="0" width="100%">
-             <caption>
-             <div className="row" style={{  display: 'flex', float:'left'}}>
-             <a className="btn btn-primary" style={{ marginLeft:'10px', marginRight:'10px' }} 
-             onClick={() => addMedicine()} >
-             <i className="fa fa-plus"></i></a>			 
-              
-             
-        <div className="search" style={{ marginRight:'10px' }}>  	        		
-		              	<input type="text" className="form-control"
-						   placeholder="Search" 
-                           onChange={filterFunction} 
-               /> 
-                 
-               </div>
-               </div> 
-
-             
-             
-               <div className="pagination-wrap" style={{ float:'right'}}>
-            <ul className="pagination">
-            <li className="wrapperdisabled">
-                <a 
-                onClick={() => nextPage(pages.prev_page_url)} 
-                className={pages.current_page == 1 ? 'disabled' : ''} aria-label="Next">
-                  <span aria-hidden="true"><i className="fa fa-backward"></i></span>
-                </a>
-              </li>
-              <li className="active"><a href="#">{pages.current_page}</a></li>
-              <li className="wrapperdisabled">
-                <a 
-                onClick={() => nextPage(pages.next_page_url)} 
-                className={pages.current_page == pages.last_page ? 'disabled' : ''} aria-label="Next">
-                  <span aria-hidden="true"><i className="fa fa-forward"></i></span>
-                </a>
-              </li>
-            </ul>
-          </div>  
-               </caption>
-                <thead>
-                  <tr>
-                    <th><a onClick={() => sortTable(0)} 
-                    className="white-text templatemo-sort-by"># <span className="caret"></span></a>
-                    </th>
-                    <th>
-                    Medicine image</th>
-                    <th><a onClick={() => sortTable(2)} 
-                     className="white-text templatemo-sort-by">
-                    Name <span className="caret"></span></a></th>
-                    <th><a onClick={() => sortTable(3)}  className="white-text templatemo-sort-by">
-                    Medicine description <span className="caret"></span></a></th>
-                    <th><a onClick={() => sortTable(4)}  className="white-text templatemo-sort-by">
-                    Ingredients <span className="caret"></span></a></th>
-                    <th><a onClick={() => sortTable(5)}  className="white-text templatemo-sort-by">
-                    Format <span className="caret"></span></a></th>
-                    <th ><a onClick={() => sortTable(6)}  className="white-text templatemo-sort-by">
-                    Prescription <span className="caret"></span></a></th>
-                    <th><a onClick={() => sortTable(7)}  className="white-text templatemo-sort-by">
-                    Tablet <span className="caret"></span></a></th>
-                    <th><a onClick={() => sortTable(8)}  className="white-text templatemo-sort-by">
-                    Dosage <span className="caret"></span></a></th>
-                    <td>Action</td>
-                  </tr>
-                </thead>
-                <tbody>
-
-                {medicine.length > 0 ? renderMedicine() : 
-                <tr><td colSpan="7" style={{ textAlign:'center' }} >no data</td></tr>}
-                </tbody>
-              </table>    
-            </div> 
-                                     
-          </div>   
-          <div id="addmedicine" style={{ display: 'none'}}>
-              <AddMedicine />
-
-            </div>
-
-  
-       </div>                       
-            </div> 
-
-            
-                     
-             <Footer />
-          </div>
-
+      return(
+        <tr key={medicine.id}>
+          <td>{medicine.id}</td>
+          <td>
           
-        </div>
+            <img src={medicine.image} 
+            style={{objectFit: 'contain' }} width="100px" height="100px"/>
+          </td>
+          <td>{medicine.name}</td>
+          <td>
+            <div style={{ overflowY:'auto', width:'200px' , height: '100px', whiteSpace: 'pre-line' }}>
+            {medicine.description}
+            </div>
+          </td>
+          <td>
+            <div style={{ overflowY:'auto', width:'200px', height: '100px', whiteSpace: 'pre-line' }}>
 
-    )
+              {medicine.ingredient}
+            </div>
+          </td>
+          <td>{medicine.format}</td>
+          <td>{medicine.prescription == 1 ? 'yes' : 'no'}</td>
+          <td>{medicine.tablet}</td>
+          <td>{medicine.dosage} {medicine.dosage_unit}</td>
+          <td style={{ textAlign:'center'}}>
+            <div className="btn-group " role="group"> 
+              <a href={`/manageMedicine/show/${medicine.id}`}
+              className="btn btn-info"
+                  title="Show">
+                  <i className="fa fa-info fa-fw"></i>
+              </a>
+              <a href={`/manageMedicine/edit/${medicine.id}`}
+              className="btn btn-primary"
+                  title="Edit">
+                  <i className="fa fa-edit fa-fw"></i>
+              </a>
+          
+              <a  onClick= {() => handleDeleteMedicine(medicine.id)}
+                className="btn btn-default"
+                  title="Delete">
+                  <i className="fa fa-trash fa-fw"></i>
+              </a>
+            </div>
+          </td>
+        </tr>
+      )
+    })
+  }
+
+  return(
+    <div className="templatemo-flex-row">
+	    <div className="templatemo-content col-1 light-gray-bg">
+        <Header />
+        <div className="templatemo-flex-row flex-content-row " style={{ marginTop:'100px' }}>
+          <div className="col-1">	
+            <div className="templatemo-content-widget no-padding">
+              <div className="panel panel-default table-responsive">
+                <table id="myTable" className="table table-striped table-bordered templatemo-user-table"
+                cellSpacing="0" width="100%">
+                  <caption>
+                    <div className="row" style={{  display: 'flex', float:'left'}}>
+                      <a className="btn btn-primary" style={{ marginLeft:'10px', marginRight:'10px' }} 
+                      onClick={() => addMedicine()} >
+                      <i className="fa fa-plus"></i></a>			 
+                        
+                      
+                       <div className="search" style={{ marginRight:'10px' }}>  	        		
+                          <input type="text" className="form-control"
+                          placeholder="Search" onChange={filterFunction} />  
+                        </div>
+                    </div> 
+                    <div className="pagination-wrap" style={{ float:'right'}}>
+                      <ul className="pagination">
+                      <li className="wrapperdisabled">
+                          <a 
+                          onClick={() => nextPage(pages.prev_page_url)} 
+                          className={pages.current_page == 1 ? 'disabled' : ''} aria-label="Next">
+                            <span aria-hidden="true"><i className="fa fa-backward"></i></span>
+                          </a>
+                        </li>
+                        <li className="active"><a href="#">{pages.current_page}</a></li>
+                        <li className="wrapperdisabled">
+                          <a 
+                          onClick={() => nextPage(pages.next_page_url)} 
+                          className={pages.current_page == pages.last_page ? 'disabled' : ''} aria-label="Next">
+                            <span aria-hidden="true"><i className="fa fa-forward"></i></span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>  
+                  </caption>
+                  <thead>
+                    <tr>
+                      <th><a onClick={() => sortTable(0)} 
+                      className="white-text templatemo-sort-by">
+                      # <span className="caret"></span></a>
+                      </th>
+                      <th>
+                      Medicine image</th>
+                      <th><a onClick={() => sortTable(2)} 
+                      className="white-text templatemo-sort-by">
+                      Name <span className="caret"></span></a>
+                      </th>
+                      <th><a onClick={() => sortTable(3)}  
+                      className="white-text templatemo-sort-by">
+                      Medicine description <span className="caret"></span></a>
+                      </th>
+                      <th><a onClick={() => sortTable(4)} 
+                      className="white-text templatemo-sort-by">
+                      Ingredients <span className="caret"></span></a>
+                      </th>
+                      <th><a onClick={() => sortTable(5)}  
+                      className="white-text templatemo-sort-by">
+                      Format <span className="caret"></span></a>
+                      </th>
+                      <th ><a onClick={() => sortTable(6)}  
+                      className="white-text templatemo-sort-by">
+                      Prescription <span className="caret"></span></a>
+                      </th>
+                      <th><a onClick={() => sortTable(7)}  
+                      className="white-text templatemo-sort-by">
+                      Tablet <span className="caret"></span></a>
+                      </th>
+                      <th><a onClick={() => sortTable(8)}  
+                      className="white-text templatemo-sort-by">
+                      Dosage <span className="caret"></span></a>
+                      </th>
+                      <td>Action</td>
+                    </tr>
+                 </thead>
+                 <tbody>
+
+                    {medicine.length > 0 ? renderMedicine() : 
+                    <tr><td colSpan="7" style={{ textAlign:'center' }} >no data</td></tr>}
+                 </tbody>
+               </table>    
+             </div> 
+                                     
+            </div>   
+            <div id="addmedicine" style={{ display: 'none'}}>
+              <AddMedicine />
+            </div>
+          </div>                       
+        </div> 
+        <Footer />
+      </div>
+   </div>
+
+  )
 
 }
