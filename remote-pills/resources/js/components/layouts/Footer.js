d08bca43edb61be329849bb193a55193d0824b85
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-25 16:01:42 +0200

    Footer

--- remote-pills/resources/js/components/layouts/Footer.js
+++ remote-pills/resources/js/components/layouts/Footer.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 export default function Footer(){
-        return (
+       return (
             
-            <footer className="text-right">
-
-
-            <p>Copyright &copy; 2084 Remote Pills | Designed by <a href="https://www.linkedin.com/in/ghada-jaber/" target="_blank">Ghada Jaber</a></p>
-                     
-                     
-                      </footer>  
-          
-          
-
+                <footer className="text-right">
+                        <p>Copyright &copy; 2084 Remote Pills | Designed by <a href="https://www.linkedin.com/in/ghada-jaber/" target="_blank">Ghada Jaber</a></p>
+                </footer>  
         )
-
 }
 
 
