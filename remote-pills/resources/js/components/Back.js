353fae24bc5aeb6d01cdc1795e5eb201a60f353f
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-25 21:20:56 +0200

    Back

--- remote-pills/resources/js/components/Back.js
+++ remote-pills/resources/js/components/Back.js
@@ -2,19 +2,17 @@ import React, {useState, useEffect} from 'react';
 import {Link, useHistory} from 'react-router-dom';
 
 export default function Back(){
-    const history = useHistory();
-    function goBack(){
-        history.goBack();
-    }
-    return (
-        <div className="container">
-        <div className="row">
-          <a className='btn btn-primary' onClick={goBack}>
-                    <i className="fa fa-arrow-left"></i>
-                </a>  
-        </div>
-
-        
+  const history = useHistory();
+  function goBack(){
+    history.goBack();
+  }
+  return (
+    <div className="container">
+      <div className="row">
+        <a className='btn btn-primary' onClick={goBack}>
+          <i className="fa fa-arrow-left"></i>
+        </a>  
       </div>
-    )
+    </div>
+  )
 }
