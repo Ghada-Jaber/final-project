2530a9c43313362f1e0cae08f76e3c18217ace48
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-25 15:52:55 +0200

    firebase

--- remote-pills/resources/js/components/firebase/init-fcm.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase";
-import "firebase/messaging";
-import config from './config';
-
-const initializedFirebaseApp = firebase.initializeApp(config);
-const messaging = initializedFirebaseApp.messaging();
-messaging.usePublicVapidKey("BGvosei8DMRjMBui54X_u43QtHf4k11Ia30F-O9coBaBX2NSgw5cug0DrAP695hLfArJwPpQ6gMRpGLuR8DT6ME");
-      
-export { messaging };
