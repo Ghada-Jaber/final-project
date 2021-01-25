a519e10a99c8192b89321477558971f2edf3ad12
Ghada-Jaber <ghadajaber94@gmail.com>
2021-01-25 09:26:23 +0200

    SHow User

--- remote-pills/resources/js/components/Admin/ShowUser.js
+++ remote-pills/resources/js/components/Admin/ShowUser.js
@@ -7,83 +7,61 @@ import Back from '../Back';
 
 export  default function ShowUser(props){
 
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [open, setOpened] = useState('');
-    const [email, setEmail] = useState('');
-    const [address, setAddress] = useState('');
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [open, setOpened] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
 
-    const [medicine, setMedicine] = useState([]);
+  const [medicine, setMedicine] = useState([]);
 
   const history = useHistory();
 
   useEffect(() => {
     api.getInfoPharmacy(props.match.params.id).then(response => {
       setImage(response.data.image);
-        setName(response.data.name);
-        setEmail(response.data.email);
-        setOpened(response.data.birthday);
-        setMedicine(response.data.medicine);
-        setAddress(response.data.street.name+", "+response.data.street.city.name+", "+response.data.street.city.country.name)
-      })
+      setName(response.data.name);
+      setEmail(response.data.email);
+      setOpened(response.data.birthday);
+      setMedicine(response.data.medicine);
+      setAddress(response.data.street.name+", "+response.data.street.city.name+", "+response.data.street.city.country.name)
+    })
 
   },[]);
 
 
-  function renderPharmacy(){
-    return medicine.map(medicine => {
-      return(
-      <li  key={medicine.id}>
-      {medicine.name}
-      </li> 
 
-        )
-      })
-  }
-
-
-    return(
-      <div className="templatemo-flex-row">
-	  
-      <div className="templatemo-content col-1 light-gray-bg">
-      
-       <Header />
+  return(
+    <div className="templatemo-flex-row">
+	    <div className="templatemo-content col-1 light-gray-bg">
+        <Header />
         <div className="templatemo-flex-row flex-content-row " style={{ marginTop:'100px' }}>
-            <div className="col-1">	
-
+          <div className="col-1">	
             <Back  />
+            <div className="container">
+              <div className="row">
+                <div className="col-md-4" style={{ top:'10px', left:'-2%'}}>
+                  <img src={image} alt="Image" width="50px" height="50px" className="img-fluid p-5" />
+                  <h2 className="text-black">{name}</h2>
 
-  <div className="container">
-      <div className="row">
-      <div className="col-md-4" style={{ top:'10px', left:'-2%'}}>
-        <img src={image} alt="Image" width="50px" height="50px" className="img-fluid p-5" />
-          <h2 className="text-black">{name}</h2>
+                  <h3>email</h3>
+                  <p>{email}</p>
 
-          <h3>email</h3>
-          <p>{email}</p>
+                  <h3>Address</h3>
+                  <p>{address}</p>
 
-          <h3>Address</h3>
-          <p>{address}</p>
+                  <h3>Opened</h3>
+                  <p>{open}</p>
+                </div>
+              </div>
 
-          <h3>Opened</h3>
-          <p>{open}</p>
-
-  
-        </div>
+            </div>
+          </div> 
+        </div>                
+        <Footer />
       </div>
-   
     </div>
-     </div> 
-     </div> 
 
-            
-                     
-<Footer />
-</div>
-
-
-</div>
-
-    )
+  )
 
 }
